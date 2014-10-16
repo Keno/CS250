5865c43b99d3acee956d610475445894851397f6
Keno Fischer <kfischer@college.harvard.edu>
2014-10-16 19:31:30 -0400

    Do Pset 3

--- pset3/pset3.v
+++ pset3/pset3.v
@@ -416,6 +416,7 @@ Qed.
    I first need to import a couple of libraries. *)
 Require Import Program.
 Require Import Wf.
+Require Import Recdef.
 
 (* The [Program Fixpoint] construct is similar to the [Fixpoint]
    construct.  The big difference is that I'm required to state
@@ -423,23 +424,16 @@ Require Import Wf.
    down.  In this case, the length of the argument list is always
    going down when we do a recursive call.
 *)
-Program Fixpoint 
-  merge_iter (xs : list (list nat)) {measure (length xs)} : list nat :=
+Function merge_iter (xs : list (list nat)) {measure length} : 
+  list nat :=
   match xs with 
-    | nil => nil
+   | nil => nil
     | h::nil => h
     | h1::h2::xs' => merge_iter (merge_pairs (h1::h2::xs'))
   end.
-(* Notice that Coq spits out a bunch of stuff here and says
-   there's 1 obligation remaining.  This obligation arises
-   as a result of the recursive call -- we need to show that
-   the measure is actually decreasing.  The command [Next Obligation]
-   let's us provide a proof of the needed fact.  Note that
-   until we finish the proofs of all obligations, the 
-   function [merge_iter] is not defined. *)
-Next Obligation.
-  apply (merge_pairs_length h1 h2 xs'). 
-Qed.
+intros.
+apply merge_pairs_length.
+Defined.
 
 Print merge_iter.
 
@@ -493,31 +487,516 @@ Eval compute in merge_sort [3;2;7;8].
 1. Show that
 
    a. sorted xs -> sorted ys -> sorted (merge xs ys)
+
+*)
+
+Lemma merge_list_all : forall P xs ys, list_all P xs -> list_all P ys -> list_all P (merge xs ys).
+intros P xs.
+induction xs; intros.
+simpl. assumption.
+simpl merge.
+induction ys.
+assumption.
+remember (nat_lte a a0) as cond. destruct cond.
+simpl. destruct H. split. assumption. eapply IHxs; assumption.
+destruct H0. apply IHys in H1.
+simpl. split; assumption.
+Qed.
+
+Lemma list_all_le : forall a n ys, a <= n -> list_all (le n) ys -> list_all (le a) ys.
+intros.
+induction ys.
+simpl. reflexivity.
+destruct H0.
+apply IHys in H1.
+simpl. split. rewrite H0 in H. assumption.
+assumption.
+Qed.
+
+Theorem merged_sorted : forall xs ys, sorted xs -> sorted ys -> sorted (merge xs ys).
+  intro xs.
+  induction xs; intros.
+  simpl. assumption.
+  induction ys. assumption.
+  simpl merge.
+  destruct (nat_lte a a0); simpl; split.
+  (* a <= a0 *)
+      eapply IHxs.
+      crush. crush.
+      eapply merge_list_all.
+      crush.
+      simpl.
+      split. assumption. apply list_all_le with (n:=a0); crush.
+  (* a > a0 *)
+    simpl in H0.
+    destruct H0.
+    apply IHys in H0.
+    simpl in H0.
+    assumption.
+    simpl in H0.
+    eapply merge_list_all with (P:=(le a0)) (xs:=(a :: xs)) (ys:= ys); intros.
+    simpl.
+    split.
+    crush.
+    simpl in H. destruct H. apply list_all_le with (n:=a); crush.
+    crush.
+Qed.
+
+(*
    b. (length xs + length ys) = length (merge xs ys)
+*)
+
+Require Import Arith.
+
+Theorem merge_adds : forall xs ys, (length xs + length ys) = length (merge xs ys).
+  induction xs. crush.
+  induction ys. crush.
+  simpl. destruct (nat_lte a a0).
+  simpl. rewrite <- IHxs with (ys:=(a0 :: ys)).
+  crush. simpl in *. rewrite <- IHys. crush.
+Qed.
+
+(*
    c. In x xs \/ In x ys <-> In (merge xs ys)
+*)
+
+Theorem merged_in : forall x xs ys, In x xs \/ In x ys <-> In x (merge xs ys).
+  induction xs. crush.
+  induction ys. crush.
+  simpl. destruct (nat_lte a a0).
+  simpl. rewrite <- IHxs with (ys:=(a0 :: ys)). crush. 
+  simpl in *. rewrite <- IHys. crush.
+Qed.
+
+(*
 
 2. Show that 
 
    a. list_all sorted xs -> list_all sorted (merge_pairs xs)
+
+*)
+
+Require Import Arith.Even.
+
+Lemma length0 : forall A (xs : list A), length xs = 0 <-> xs = [].
+induction xs. crush. simpl. split; crush.
+Qed.
+
+Lemma length0' : forall A (xs : list A), 0 = length xs <-> xs = [].
+induction xs. crush. simpl. split; crush.
+Qed.
+
+Lemma not_odd_and_even : forall n, odd n -> even n -> False.
+crush. apply not_even_and_odd with (n:=n); assumption.
+Qed.
+
+Lemma oddS : forall m, odd (S m) -> even m.
+intros. destruct m.
+crush. specialize even_odd_dec with (n:=(S m)).
+intros. destruct H0. assumption. contradict H.
+intro. apply not_even_and_odd with (n:=(S (S m))).
+constructor. assumption. assumption.
+Qed.
+
+Lemma evenS : forall m, even (S m) -> odd m.
+intros. induction m.
+contradict H. crush. assert (odd 1). crush.
+apply not_even_and_odd with (n:=1); assumption.
+specialize even_odd_dec with (n:=(S m)).
+intros. destruct H0. apply IHm in e.
+contradict H. intro. apply not_even_and_odd with (n:=(S (S m))).
+assumption. constructor. constructor. assumption. assumption.
+Qed.
+ 
+Lemma evenSn: forall m, (even m -> (exists n, m = 2*n)) /\ (odd m -> exists n, m = S(2*n)).
+induction m.
+split. intros. exists 0. simpl. reflexivity.
+intro. contradict H. crush. assert (even 0). crush. 
+apply not_even_and_odd with (n:=0); assumption.
+split. destruct IHm.
+intro. apply evenS in H1. apply H0 in H1.
+destruct H1. rewrite H1. exists (S x). crush.
+destruct IHm. intro. apply oddS in H1. apply H in H1.
+destruct H1. rewrite H1. exists x. crush.
+Qed.
+
+Lemma evenSn2: forall m, (exists n, m = 2*n) -> even m.
+intros. destruct H. simpl in H. assert( x + (x + 0) = x+x). crush. rewrite H0 in H. clear H0.
+specialize even_odd_dec with (n:=x).
+intros. rewrite H. destruct H0.
+apply even_even_plus; assumption.
+apply odd_even_plus; assumption.
+Qed.
+
+Lemma test: forall A (xs: list A), even (length xs) -> (length xs <> 0) -> (exists a b t, (xs = a :: b :: t) /\ (even (length t))).
+intros.
+destruct xs. contradict H0.
+simpl. reflexivity.
+destruct xs.
+contradict H.
+simpl. intro.
+assert (odd 1). crush.
+apply not_even_and_odd with (n:=1); assumption.
+exists a. exists a0. exists xs. split. reflexivity.
+simpl in H. apply evenS in H. apply oddS in H. assumption.
+Qed.
+
+Lemma neq : forall n, n<>0 <-> exists m, n = S m.
+crush. destruct n. crush. exists n. reflexivity. Qed.
+
+Lemma list_all_tail : forall A P (l:A) (r: list A), list_all P r /\ P l <-> list_all P (r ++ [l]).
+intros. induction r; simpl; crush.
+Qed.
+
+Lemma app_cons : forall A (x:A) xs, x :: xs = [x] ++ xs.
+crush.
+Qed.
+
+Lemma merge_pairs_app: forall xs l, even (length xs) -> merge_pairs (xs ++ [l]) = (merge_pairs xs) ++ [l].
+  Lemma merge_pairs_app': forall n xs l, (n*2) = length xs -> merge_pairs (xs ++ [l]) = (merge_pairs xs) ++ [l].
+    induction n.
+    intros. simpl in H. apply eq_sym in H. rewrite length0 in H. rewrite H.
+    simpl. reflexivity.
+    intros.
+    pose proof test.
+    assert (even (length xs)). apply evenSn2. exists (S n).
+    rewrite mult_comm. apply eq_sym. assumption.
+    specialize H0 with (xs:=xs).
+    apply H0 in H1. destruct H1. destruct H1. destruct H1.
+    destruct H1. rewrite H1. simpl.
+    assert (merge_pairs x1 ++ [l] = merge_pairs (x1 ++ [l])).
+    apply eq_sym. apply IHn.
+    rewrite H1 in H. simpl in H. injection H. intros. assumption.
+    rewrite H3. reflexivity.
+    apply neq. rewrite <- H. simpl. exists (S (n * 2)). reflexivity.
+  Qed.
+  intros. apply evenSn in H. destruct H.
+  apply merge_pairs_app' with (n:=x). crush.
+Qed.
+
+
+Lemma odd0 : odd 0 -> False. intro. assert (even 0). crush. apply not_even_and_odd with (n:=0); assumption. Qed.
+
+Ltac mymagic := match goal with
+  | [ H: odd 0 |- _ ] => apply odd0 in H; crush
+  | [ H: odd (length []) |- _ ] => simpl in H; apply odd0 in H; crush
+  | [ H: context[ _ + length [_]] |- _ ] => simpl length in H; rewrite plus_comm in H; simpl plus in H
+  | [ H: context[(length (_ ++ [_]))] |- _ ] => rewrite app_length in H; mymagic
+  | [ H: 0 = length ?xs |- _ ] => apply eq_sym in H; apply length0 in H
+end.
+
+Lemma evenSn' : forall A (Q: list A -> Prop), (forall n (xs : list A), 2*n = length xs -> Q xs) <-> (forall (xs : list A), (even (length xs)) -> Q xs).
+  intros. split.
+  intros. apply evenSn in H0.  destruct H0. apply eq_sym in H0. apply H in H0. assumption.
+  intros. eapply H. apply evenSn2. exists n. crush.
+Qed.
+
+Lemma list_odd : forall A (xs:list A), (odd (length xs)) -> (exists (ys:list A) (t:A), xs = ys ++ [t] /\ even (length ys)).
+  intros.
+  assert (xs <> []). crush. mymagic.
+  exists (removelast xs). apply exists_last in H0.
+  destruct H0. destruct s. exists x0. rewrite e. rewrite removelast_app. simpl.
+  rewrite app_nil_r. split. reflexivity.
+  rewrite e in H. mymagic. apply oddS in H. assumption. crush.
+Qed.
+
+Ltac third := let HH:=fresh in let H2:=fresh "X" in intro n; induction n; [
+        (* Base Case *)
+        simpl mult; try (split; intros x HH); try rewrite length0' in HH; subst; simpl in *; [ crush; try mymagic; crush | .. ]; try assumption |
+        (* Start of Recursive Case *)
+        intros xs Hy;
+        remember (2 * S n) as m;
+        assert (even m /\ m <> 0) as Hx; [ split; [ apply evenSn2; exists (S n); assumption | crush ] | ];
+        specialize test with (xs:=xs); intros Hm; rewrite <- Hy in Hm;
+        destruct Hx; destruct Hm as [x H2]; [ assumption | assumption | .. ] ];
+        destruct H2 as [y H2]; destruct H2 as [z H2]; destruct H2; subst.
+
+Theorem merge_pairs_sorted : forall xs, list_all sorted xs -> list_all sorted (merge_pairs xs).
+  Lemma merge_pairs_sorted_even : forall xs, (even (length xs)) -> list_all sorted xs -> list_all sorted (merge_pairs xs).
+    erewrite <- evenSn'. third. crush. apply merged_sorted; assumption.
+
+  Qed.
+  intros.
+  remember (length xs) as n.
+  specialize even_odd_dec with (n:=n). intros Hx. destruct Hx. 
+   + apply merge_pairs_sorted_even; [ crush | .. ]; try assumption.
+   + specialize list_odd with (xs:=xs). intros Hy. subst. apply Hy in o.
+     destruct o eqn:Hz. destruct Hz eqn:Ha. destruct e eqn:Hb. crush.
+     rewrite merge_pairs_app; [ | assumption ].
+     (* apply eq_sym. rewrite fold_right_app. *)
+     rewrite H1 in H. apply list_all_tail in H. destruct H.
+     apply list_all_tail; (try split); try assumption.
+     apply merge_pairs_sorted_even. assumption. assumption.
+Qed.
+
+(*
    b. (sum of lengths of lists in xs) = 
         (sum of lengths of lists in merge_pairs xs)
+*)
+
+Definition sum_length (a:list nat) (b:nat) := (plus (length a) b).
+
+Hint Unfold sum_length.
+
+Theorem sum_merge_pairs : forall (xs: list (list nat)), (fold_right sum_length 0 xs) = (fold_right sum_length 0 (merge_pairs xs)).
+  Lemma sum_merge_pairs_even : forall d xs, (even (length xs)) -> (fold_right sum_length d xs) = (fold_right sum_length d (merge_pairs xs)).
+    intro d. erewrite <- evenSn'.
+    third. crush. unfold sum_length. fold sum_length. rewrite <- merge_adds. erewrite <- IHn; [ | crush]. crush.
+  Qed.
+  intros.
+  remember (length xs) as n.
+  specialize even_odd_dec with (n:=n). intros Hx. destruct Hx. 
+   + apply sum_merge_pairs_even; [ crush | .. ]; try assumption.
+   + specialize list_odd with (xs:=xs). intros Hy. subst. apply Hy in o.
+     destruct o eqn:Hz. destruct Hz eqn:Ha. destruct e eqn:Hb. 
+     destruct a as [aa ab]. rewrite aa.
+     rewrite merge_pairs_app; [ | assumption ].
+     (* End Generic Skeleton *)
+     rewrite fold_right_app. apply eq_sym. rewrite fold_right_app.
+     rewrite <- sum_merge_pairs_even; [ | assumption ].
+     subst. reflexivity.
+Qed.
+
+(*
    c. (x is in one of the lists in xs) <->
         (x is in one of the lists in merge_pairs xs)
+
+*)
+Definition InOneOf (x:nat) (xs: list (list nat)) : Prop :=
+ exists subl, (In x subl) /\ (In subl xs).
+
+Hint Unfold InOneOf.
+
+Lemma In_app : forall A (x: A) xs ys, In x (xs ++ ys) <-> In x xs \/ In x ys.
+  intros. induction xs; crush.
+Qed.
+
+Lemma InOneOf_app : forall x xs ys, InOneOf x (xs ++ ys) <-> InOneOf x xs \/ InOneOf x ys.
+  intros. unfold InOneOf.
+  split; intros.
+  do 2 destruct H; rewrite In_app in H0. 
+  { destruct H0.
+    + left. exists x0. split; assumption.
+    + right. exists x0. split; assumption. 
+  }
+  do 2 destruct H; exists x0; rewrite In_app; crush.
+Qed.
+
+Lemma InOneOf_one : forall x y, InOneOf x [y] <-> In x y.
+  crush. unfold InOneOf in H. crush. unfold InOneOf. exists y. crush.
+Qed.
+
+Lemma InOneOf_cons : forall x xs y, InOneOf x (y :: xs) <-> In x y \/ InOneOf x xs.
+  intros. rewrite app_cons. rewrite <- InOneOf_one. eapply InOneOf_app.
+Qed. 
+
+Theorem InPairs : forall (x:nat) (xs: list (list nat)), InOneOf x xs <-> InOneOf x (merge_pairs xs).
+  Lemma InPairs_even : forall x xs, (even (length xs)) -> (InOneOf x xs <-> InOneOf x (merge_pairs xs)).
+     intro d. erewrite <- evenSn'. third.
+     simpl merge_pairs. repeat rewrite InOneOf_cons.
+     rewrite <- merged_in. rewrite IHn; [ | crush ].
+     crush.
+  Qed.
+  intros.
+  remember (length xs) as n.
+  specialize even_odd_dec with (n:=n). intros Hx. destruct Hx. 
+   + apply InPairs_even; [ crush | .. ]; try assumption.
+   + specialize list_odd with (xs:=xs). intros Hy. subst. apply Hy in o.
+     destruct o eqn:Hz. destruct Hz eqn:Ha. destruct e eqn:Hb.
+     destruct a as [aa ab]. rewrite aa.
+     rewrite merge_pairs_app; [ | assumption ].
+     (* End Generic Skeleton *)
+     rewrite InOneOf_app. apply iff_sym. rewrite InOneOf_app. 
+     rewrite InPairs_even with (xs:=x0); [ | assumption ].
+     reflexivity.
+Qed.
+
+(*
 
 3. Show that
 
    a. list_all sorted xs -> sorted (merge_iter xs)
+
+*)
+
+Theorem merge_iter_sorted : forall xs, list_all sorted xs -> sorted (merge_iter xs).
+intros.
+functional induction (merge_iter xs).
+crush. crush. apply merge_pairs_sorted in H. apply IHl in H. assumption.
+Qed.
+
+(*
    b. (sum of lengths of lists in xs) = length of (merge_iter xs)
+*)
+
+Theorem merge_iter_length : forall xs, (fold_right sum_length 0 xs) = length (merge_iter xs).
+intros.
+functional induction (merge_iter xs).
+crush. crush. unfold sum_length. crush.
+rewrite sum_merge_pairs. assumption.
+Qed.
+
+(*
    c. (x is in one of the lists in xs) <-> In x (merge_iter xs)
+*)
 
+Theorem merge_iter_in : forall x xs, InOneOf x xs <-> In x (merge_iter xs).
+intros.
+functional induction (merge_iter xs).
+crush. unfold InOneOf in H. destruct H. crush.
+crush. unfold InOneOf in H. crush.
+unfold InOneOf. exists h. crush.
+rewrite InPairs. assumption.
+Qed.
+
+(*
 4.  Show that
-
     a. make_lists xs is sorted
     b. In x xs <-> (x is in one of the lists in make_lists xs)
     c. length xs = (sum of the lengths of the lists in make_lists xs)
+*)
+
+Theorem make_lists_sorted : forall xs, list_all sorted (make_lists xs).
+induction xs. crush. simpl. crush.
+Qed.
+
+Theorem make_lists_in : forall x xs, In x xs <-> InOneOf x (make_lists xs).
+intros. induction xs.
+crush. unfold InOneOf in H. destruct H. crush.
+simpl. rewrite app_cons. rewrite InOneOf_app.
+split. intros.
+destruct H. left. rewrite H. unfold InOneOf. exists [x]. crush.
+apply IHxs in H. right. assumption.
+intros. destruct H. left. unfold InOneOf in H. destruct H. crush. crush.
+Qed.
+
+Theorem make_lists_length : forall xs, length xs = (fold_right sum_length 0 (make_lists xs)).
+intros. induction xs. crush.
+simpl. unfold sum_length. fold sum_length. crush.
+Qed.
+
+ (*
 
 5.  Show that sort_corr xs (merge_sort xs)
+
+*)
+
+Theorem merge_corr: forall xs, sort_corr xs (merge_sort xs).
+crush. unfold sort_corr. unfold merge_sort.
+split. apply merge_iter_sorted. apply make_lists_sorted.
+split. unfold sublist. intros.
+apply merge_iter_in. apply make_lists_in. assumption.
+rewrite <- merge_iter_length. apply make_lists_length.
+Qed.
+
+(*
 
 6.  Define a better definition of correctness for sorts (sort_corr').
 
 *)
+
+Require Import Permutation.
+
+Definition sort_corr' (xs ys:list nat) : Prop :=
+  sorted ys /\ Permutation xs ys.
+
+(* This is better because *)
+Goal sort_corr ([1;1;2;2;3;3]) ([1;1;2;2;2;3]).
+  unfold sort_corr.
+  unfold sorted.
+  unfold sublist.
+  simpl.
+  crush.
+Qed.
+
+Goal ~ sort_corr' ([1;1;2;2;3;3]) ([1;1;2;2;2;3]). 
+  unfold sort_corr'. crush. clear -H1.
+  repeat apply Permutation_cons_inv in H1.
+  apply Permutation_length_2_inv in H1.
+  destruct H1; crush.
+Qed.
+
+(* Let's show that this is a stronger condition *)
+Theorem sort_corr'_strong : forall xs ys, sort_corr' xs ys -> sort_corr xs ys.
+  intros. unfold sort_corr. unfold sort_corr' in H. crush.
+  - unfold sublist. intros. apply Permutation_in with (l:=xs); assumption.
+  - apply Permutation_length; assumption.
+Qed.
+
+(* And let's show that merge still satisfies this *)
+Fixpoint flatten {A: Type} (xs: list (list A)) : list A :=
+  match xs with
+   | [] => []
+   | A::B => (app A (flatten B))
+  end.
+
+Lemma flatten_app : forall x (xs: list (list nat)), flatten (xs ++ [x]) = (flatten xs) ++ x.
+  intros. induction xs; crush.
+Qed.
+
+Lemma make_lists_inv : forall xs, xs = (flatten (make_lists xs)).
+  induction xs. crush. crush.
+Qed.
+
+Theorem merge_pairs_permutation : forall xs, Permutation (flatten xs) (flatten (merge_pairs xs)).
+Lemma merge_pairs_permutation_even : forall xs, even (length xs) -> Permutation (flatten xs) (flatten (merge_pairs xs)).
+  Lemma merge_pairs_permutation_even' : forall n xs, 2*n=length xs -> Permutation (flatten xs) (flatten (merge_pairs xs)).
+    Lemma merge_permutation : forall xs ys, Permutation (xs++ys) (merge xs ys).
+      induction xs. crush.
+      induction ys. 
+      - crush. apply perm_skip. specialize IHxs with (ys:=[]). 
+        rewrite app_nil_r in IHxs. apply Permutation_sym. assumption.
+      - simpl. destruct (nat_lte a a0).
+        apply perm_skip. eapply IHxs.
+        apply Permutation_trans with (l' := (a0 :: a :: xs ++ ys)).
+        + admit.
+        + unfold merge in IHxs. fold merge in IHxs. apply perm_skip. apply IHys.
+    Qed.
+    third.
+    specialize IHn with (xs:=z).
+    assert (2*n = length z). crush.
+    apply IHn in H1. clear IHn.
+    crush.
+    rewrite app_assoc.
+    apply Permutation_app.
+    apply merge_permutation.
+    assumption.
+  Qed.
+  erewrite <- evenSn'. apply merge_pairs_permutation_even'.
+  Qed.
+  Show.
+  intros.
+  remember (length xs) as n.
+  specialize even_odd_dec with (n:=n). intros Hx.
+  destruct Hx. apply merge_pairs_permutation_even. crush.
+  specialize list_odd with (xs:=xs). intros Hy. subst. apply Hy in o.
+  destruct o eqn:Hz. destruct Hz eqn:Ha. destruct e eqn:Hb. crush.
+  rewrite merge_pairs_app.
+  remember (fold_right sum_length 0 (x1 ++ [x2])).
+  do 2 rewrite flatten_app.
+  apply Permutation_app_tail.
+  apply merge_pairs_permutation_even. assumption.
+  assumption.
+Qed.
+
+Theorem merge_iter_perm : forall xs, Permutation (merge_iter xs) (flatten xs).
+  crush. functional induction (merge_iter xs).
+  crush. crush. rewrite app_nil_r. apply Permutation_refl.
+  apply Permutation_trans with (l' := flatten (merge_pairs (h1 :: h2 :: xs'))).
+  assumption.
+  apply Permutation_sym. apply merge_pairs_permutation.
+Qed.
+
+Theorem sort_corr'_merge : forall xs, sort_corr' xs (merge_sort xs).
+  intros. unfold sort_corr'.
+  specialize merge_corr with (xs:=xs). 
+  split. unfold sort_corr in H. crush.
+  unfold merge_sort.
+  unfold sort_corr in H.
+  unfold merge_sort.
+  destruct (merge_sort xs).
+  crush. apply length0 in H2. crush.
+  rewrite make_lists_inv with (xs:=xs) at 1. 
+  apply Permutation_sym.
+  apply merge_iter_perm.
+Qed.
