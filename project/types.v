8a567a271524cdd14e134ec679ea7e5c341c3ce7
Jao-ke Chin-Lee <jchinlee94@gmail.com>
2014-12-02 00:45:32 -0500

    check props

--- project/types.v
+++ project/types.v
@@ -58,9 +58,9 @@ Definition Method := (prod (list JLType) ConcreteType).
 Definition MethodTable := list Method.
 
 Inductive ExprTree :=
-| Call : MethodTable -> list ExprTree -> JLType -> ExprTree
-| Const : ConcreteType -> ExprTree
-| VarLookup : var -> JLType -> ExprTree.
+| Call : MethodTable -> list ExprTree -> JLType -> ExprTree (* allowed annots, params, rettype *)
+| Const : ConcreteType -> ExprTree (* consttype *)
+| VarLookup : var -> JLType -> ExprTree. (* var, vartype *)
 
 Inductive LoweredASTNode :=
 | Assign : var -> ExprTree -> LoweredASTNode
@@ -95,21 +95,133 @@ Definition issub' (x : option JLType) (y : JLType) : Prop :=
     | Some x' => issub x' y
   end.
 
-Definition var_lookup (typs : TypedList JLType) (v : var) : JLType.
-Admitted.
+Fixpoint var_lookup (typs : TypedList JLType) (v : var) : option JLType :=
+  match typs with
+    | nil => None
+    | (u,t)::rest => if string_dec u v then Some t else var_lookup rest v
+  end.
+
+Definition var_istype (typs : TypedList JLType) (v : var) (t : JLType) : Prop :=
+  match var_lookup typs v with
+    | None => False
+    | Some t' => t' = t
+  end.
+
+Definition var_isdef (typs : TypedList JLType) (v : var) : Prop :=
+  match var_lookup typs v with
+    | None => False
+    | _ => True
+  end.
+
+Inductive PreExprTreeValid : (TypedList JLType) -> ExprTree -> ExprTree -> Prop :=
+| PreCall_valid : forall typs f args1 args2 ret1 ret2,
+                    issub' (rettype (method_lookup f args2)) ret2 ->
+                    issub ret1 ret2 -> (* rettype *)
+                    (* argtype *)
+                      PreExprTreeValid typs (Call f args1 ret1) (Call f args2 ret2)
+| PreConst_valid : forall typs C, PreExprTreeValid typs (Const C) (Const C)
+| PreVarLookup_valid : forall typs v T1 T2,
+                         (*(*var_isdef typs v ->*)
+                         var_istype typs v T1 ->*)
+                         issub T1 T2 ->
+                           PreExprTreeValid typs (VarLookup v T1) (VarLookup v T2).
+
+Lemma PreExprTreeValid_refl : forall typs tree, PreExprTreeValid typs tree tree.
+Proof.  
+  induction tree; intros; econstructor.
+  induction m; simpl in *; crush.
+  econstructor.
+  econstructor.
+Qed.
 
 Inductive ExprTreeValid : (TypedList JLType) -> ExprTree -> ExprTree -> Prop :=
-| Call_valid : forall typs M args1 args2 ret1 ret2, 
-    issub' (rettype (method_lookup M args2)) ret2 -> 
-    issub ret2 ret1 -> 
-    twolist_all (fun A B => ExprTreeValid typs A B) args1 args2 ->
-      ExprTreeValid typs (Call M args1 ret1) (Call M args2 ret2)
-| Const_valid : forall typs C, ExprTreeValid typs (Const C) (Const C)
-| VarLookup_valid : forall typs v T1 T2, issub (var_lookup typs v) T2 -> 
-     issub T2 T1 -> ExprTreeValid typs (VarLookup v T1) (VarLookup v T2).
+| Call_valid : forall typs f args1 args2 ret1 ret2,
+                 PreExprTreeValid typs (Call f args1 ret1) (Call f args2 ret2) ->
+                 twolist_all (fun A B => PreExprTreeValid typs A B) args1 args2 ->
+                   ExprTreeValid typs (Call f args1 ret1) (Call f args2 ret2)
+| Const_valid : forall typs C,
+                  PreExprTreeValid typs (Const C) (Const C) ->
+                    ExprTreeValid typs (Const C) (Const C)
+| VarLookup_valid : forall typs v T1 T2,
+                      PreExprTreeValid typs (VarLookup v T1) (VarLookup v T2) ->
+                        ExprTreeValid typs (VarLookup v T1) (VarLookup v T2).
 
 Lemma ExprTreeValid_refl : forall typs tree, ExprTreeValid typs tree tree.
-Admitted.
+Proof.
+  induction tree; intros; econstructor.
+  eapply PreExprTreeValid_refl.
+  induction l; crush.
+  eapply PreExprTreeValid_refl.
+  eapply PreExprTreeValid_refl.
+  eapply PreExprTreeValid_refl.
+Qed.
+
+Inductive NodeInferenceValid : (TypedList JLType) -> LoweredASTNode -> LoweredASTNode -> Prop :=
+| Assign_valid : forall typs tree1 tree2 v,
+                   ExprTreeValid typs tree1 tree2 -> 
+                     NodeInferenceValid typs (Assign v tree1) (Assign v tree2)
+| GotoNode_valid : forall typs l,
+                     NodeInferenceValid typs (GotoNode l) (GotoNode l)
+| GotoIfNot_valid : forall typs tree1 tree2 l,
+                      ExprTreeValid typs tree1 tree2 ->
+                        NodeInferenceValid typs (GotoIfNot tree1 l) (GotoIfNot tree2 l)
+| Return_valid : forall typs tree1 tree2,
+                   ExprTreeValid typs tree1 tree2 ->
+                     NodeInferenceValid typs (Return tree1) (Return tree2).
+
+Lemma NodeInferenceValid_refl : forall typs node, NodeInferenceValid typs node node.
+Proof.
+  induction node; intros; econstructor; apply ExprTreeValid_refl.
+Qed.
+
+Definition InferenceValid (typs: TypedList JLType) (AST1: LoweredAST) (AST2: LoweredAST) : Prop :=
+  twolist_all (fun A B => NodeInferenceValid typs A B) AST1 AST2.
+
+Hint Unfold InferenceValid.
+
+Lemma InferenceValid_refl : forall typs AST, InferenceValid typs AST AST.
+Proof.
+  induction AST. crush. unfold InferenceValid. simpl. crush.
+  apply NodeInferenceValid_refl.
+Qed.
+
+(* PAUSE *)
+
+Inductive PreExprTreeValid : (TypedList JLType) -> ExprTree -> ExprTree -> Prop :=
+| PreCall_valid : forall typs M args1 args2 ret1 ret2, 
+    issub' (rettype (method_lookup M args2)) ret2 -> 
+    issub ret2 ret1 -> 
+    (*twolist_all (fun A B => ExprTreeValid typs A B) args1 args2 ->*)
+      PreExprTreeValid typs (Call M args1 ret1) (Call M args2 ret2)
+| PreConst_valid : forall typs C, PreExprTreeValid typs (Const C) (Const C)
+| PreVarLookup_valid : forall typs v T1 T2, issub (var_lookup typs v) T2 -> 
+     issub T2 T1 -> PreExprTreeValid typs (VarLookup v T1) (VarLookup v T2).
+
+Lemma PreExprTreeValid_refl : forall typs tree, PreExprTreeValid typs tree tree.
+Proof.
+  induction tree; intros; econstructor.
+  induction m; simpl in *; crush.
+  econstructor.
+  
+
+
+
+Inductive ExprTreeValid : (TypedList JLType) -> ExprTree -> ExprTree -> Prop :=
+| Call_valid : forall typs M args1 args2 ret1 ret2,
+                 PreExprTreeValid typs (Call M args1 ret1) (Call M args2 ret2)
+                 -> twolist_all (fun A B => PreExprTreeValid typs A B) args1 args2 ->
+                 ExprTreeValid typs (Call M args1 ret1) (Call M args2 ret2)
+| Const_valid : forall typs C,
+                  PreExprTreeValid typs (Const C) (Const C) ->
+                  ExprTreeValid typs (Const C) (Const C)
+| VarLookup_valid : forall typs v T1 T2,
+                      PreExprTreeValid typs (VarLookup v T1) (VarLookup v T2) ->
+                      ExprTreeValid typs (VarLookup v T1) (VarLookup v T2).
+
+Lemma ExprTreeValid_refl : forall typs tree, ExprTreeValid typs tree tree.
+Proof.
+  induction tree; intros; econstructor.
+  
 
 Inductive NodeInferenceValid : (TypedList JLType) -> LoweredASTNode -> LoweredASTNode -> Prop :=
 | Assign_valid : forall typs tree1 tree2 v,
